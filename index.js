5f54cc19f865719283687b71d55e6d69e8ed8ce0
Zano <zanopcwr@protonmail.com>
2022-09-18 23:44:56 -0300

    Feat: added required libs and tasks

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const test = require("./package.json")
+const test = require("./lib/Test1.json")
+const appA = require("./Applications/appA.json")
+const appB = require("./Applications/appB.json")
+const appC = require("./Applications/appC.json")
 
 
-
+console.log(test)
 
