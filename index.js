e3b57ec85e27f590edb982bd433dc579998adf02
Zano <zanopcwr@protonmail.com>
2022-09-19 15:03:31 -0300

    fix: fixed processor matrix problem, TODO: being developing app loops

--- index.js
+++ index.js
@@ -4,11 +4,46 @@ const appB = require("./Applications/appB.json")
 const appC = require("./Applications/appC.json")
 
 
-const processor = {package: 0, name: ""}
-
-const matrix = {lines: 0, column: 0, processor: [lines][column]}
 
 let matrixSizeX = test.MPSOC_SIZE_X
 let matrixSizeY = test.MPSOC_SIZE_Y
 let tasksPerProcessor = test.TASKS_PER_PROCESSOR
 let tasks = test.TEST
+
+class processor {
+    constructor() {
+        this.package = 0
+    }
+}
+
+const matrix = {sizeX: 0, sizeY: 0, processorMap: []}
+
+
+function initMatrix(mtx) {
+
+    if(matrixSizeX < 1 || matrixSizeY < 1) {
+        console.log("Eror!, matrix size is too small! Resting to 4...")
+        matrixSizeX = 4
+        matrixSizeY = 4
+    }
+
+    for(let i = 0; i < matrixSizeX; i++) {
+        mtx.processorMap.push(new Array())
+        for(let y = 0; y < matrixSizeY; y++) {
+            mtx.processorMap[i].push(new processor())
+        }
+    }
+}
+
+function getProcessor(x, y) {
+    return matrix.processorMap[x][y].package
+}
+
+function main() {
+
+    //initialize matrix
+    initMatrix(matrix)
+
+    //
+
+}
